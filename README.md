267a6e9adfc3b2dd1accf45b28cd1d67281d76e5
yscmark <124672723+yscmark@users.noreply.github.com>
2025-05-05 18:00:49 -0700

    Testing branch; should be wrong

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lab 5 - Starter
+
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+1. No. There are multiple components for the message feature, so testing the message feature as a whole would require testing all the components and how they interact with each other. A unit test is not the right choice for this case.
+
+2. Yes. The max message length feature is a single component and can be characterized as a unit.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,48 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+    // TODO
+    const synth = window.speechSynthesis;
+    let voices = [];
+    synth.addEventListener("voiceschanged", () => {
+        voices = synth.getVoices();
+        for (const voice of voices) {
+            const option = document.createElement("option");
+            option.textContent = `${voice.name} (${voice.lang})`;
+            if (voice.default) {
+                option.textContent += " - DEFAULT";
+            }
+
+            option.setAttribute("data-lang", voice.lang);
+            option.setAttribute("data-name", voice.name);
+            voiceSelect.appendChild(option);
+        }
+    });
+
+    const voiceSelect = document.getElementById("voice-select");
+    const talkButton = document.querySelector("button");
+    const textArea = document.getElementById("text-to-speak");
+    talkButton.addEventListener("click", () => {
+        const utter = new SpeechSynthesisUtterance(textArea.value);
+        const selectedOption =
+            voiceSelect.options[voiceSelect.selectedIndex].getAttribute(
+                "data-name"
+            );
+        for (const voice of voices) {
+            if (voice.name === selectedOption) {
+                utter.voice = voice;
+            }
+        }
+
+        const image = document.querySelector("img");
+        utter.onstart = () => {
+            image.src = "assets/images/smiling-open.png";
+        };
+        utter.onend = () => {
+            image.src = "assets/images/smiling.png";
+        };
+        synth.speak(utter);
+    });
 }
