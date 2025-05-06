76cb238690b06676dc1b7fc27199637bc6b21a95
yscmark <124672723+yscmark@users.noreply.github.com>
2025-05-05 19:05:51 -0700

    Re-add finished JS and edit README

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,41 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+    // TODO
+    const hornSelect = document.getElementById("horn-select");
+    const image = document.querySelector("img");
+    const audio = document.querySelector(".hidden");
+    hornSelect.addEventListener("change", () => {
+        image.src = `assets/images/${hornSelect.value}.svg`;
+        audio.src = `assets/audio/${hornSelect.value}.mp3`;
+    });
+
+    const volumeSlider = document.querySelector("#volume-controls > input");
+    const icon = document.querySelector("#volume-controls > img");
+    volumeSlider.addEventListener("input", () => {
+        const volume = volumeSlider.value;
+        let volumeLevel = 0;
+        if (volume == 0) {
+            volumeLevel = 0;
+        } else if (volume < 33) {
+            volumeLevel = 1;
+        } else if (volume < 67) {
+            volumeLevel = 2;
+        } else {
+            volumeLevel = 3;
+        }
+        icon.src = `assets/icons/volume-level-${volumeLevel}.svg`;
+        audio.volume = volume / 100;
+    });
+
+    const playButton = document.querySelector("button");
+    const jsConfetti = new JSConfetti();
+    playButton.addEventListener("click", () => {
+        audio.play();
+        if (hornSelect.value == "party-horn") {
+            jsConfetti.addConfetti();
+        }
+    });
 }
